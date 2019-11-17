fc305c7051ba7b6c16772c9c553768117b451eb5
Yuji Konishi <yuji.k64613@gmail.com>
2019-11-17 17:46:19 +0900

    test

---
+++ src/test/java/org/eclipse/jetty/embedded/FileServerTest.java
@@ -0,0 +1,12 @@
+package org.eclipse.jetty.embedded;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileServerTest {
+	@Test
+	public void test() {
+		assertTrue(false);
+    }
+}
